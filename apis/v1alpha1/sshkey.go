5dd10555dbf3ba826d729118ae4fd743a725d292
Alejandro J. Nuñez Madrazo <alejandro@civo.com>
2023-01-17 14:35:33 +0000

    Added the sshkey spec
    Signed-off-by: Alejandro J. Nuñez Madrazo <alejandro@civo.com>

---
+++ apis/v1alpha1/sshkey.go
@@ -0,0 +1,32 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// SSHKeySpec defines the desired state of SSHKey
+type SSHKeySpec struct {
+	PublicKey string `json:"public_key"`
+}
+
+// SSHKeyStatus
+type SSHKeyStatus struct {
+	State       string `json:"state,omitempty"`
+	Fingerprint string `json:"fingerprint,omitempty"`
+}
+
+// SSHKey is the Schema for the SSHKey API
+type SSHKey struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   SSHKeySpec   `json:"spec,omitempty"`
+	Status SSHKeyStatus `json:"status,omitempty"`
+}
+
+// SSHKeyList contains a list of SSHKey
+type SSHKeyList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []SSHKey `json:"items"`
+}
